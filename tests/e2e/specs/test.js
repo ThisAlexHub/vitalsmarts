e3dc149db086ae150339ed7998a343c83fcc0f8c
Bob <alex@saivian.net>
2021-07-01 11:40:22 +0300

    added User Sign in tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,5 +1,80 @@
-// https://docs.cypress.io/api/introduction/api.html
+import HomePage from '../PageObject/HomePage';
+import TestPage from '../PageObject/TestPage';
 
-//This is where you would write end-to-end tests
+
+const homePage = new HomePage();
+const testPage = new TestPage();
+
+beforeEach(() => {
+  cy.visit("http://localhost:8080/");
+
+  homePage.getTestButton().click();
+
+  testPage.getSignUpLabel().should('have.text', "Sign Up for a course");
+
+})
+
+
+
+describe('User Sign in test', function () {
+
+
+  it('User should be able to sign in with valid full name credentials and course 1 selection', function () {
+
+
+    testPage.getFullNameInput().clear();
+    testPage.getFullNameInput().type('Alex Sumak');
+    testPage.getCourseSelectionOptions().select('Course 1');
+    testPage.getAgreeCheckBox().click();
+    testPage.getSubmitButton().click();
+
+    homePage.getWelcomeLabel().should('have.text', "Welcome to the VitalSmarts QA Test");
+  })
+
+
+  it('User should be able to sign in with valid full name credentials and course 2 selection', function () {
+
+
+    testPage.getFullNameInput().clear();
+    testPage.getFullNameInput().type('Alex Sumak');
+    testPage.getCourseSelectionOptions().select('Course 2');
+
+    testPage.getAgreeCheckBox().click();
+    testPage.getSubmitButton().click();
+
+    homePage.getWelcomeLabel().should('have.text', "Welcome to the VitalSmarts QA Test");
+
+  })
+
+  it('User should not be able to sign in without full name', function () {
+
+
+    testPage.getFullNameInput().clear();
+
+    testPage.getCourseSelectionOptions().select('Course 2');
+
+    testPage.getAgreeCheckBox().click();
+    testPage.getSubmitButton().click();
+
+    // shouldn't be able to sign up 
+
+  })
+
+  it('User should not be able to sign in without course selection', function () {
+
+
+    testPage.getFullNameInput().clear();
+    testPage.getFullNameInput().type('Alex Sumak');
+
+
+    testPage.getAgreeCheckBox().click();
+    testPage.getSubmitButton().click();
+
+    // shouldn't be able to sign up 
+
+
+  })
+
+})
 
 
